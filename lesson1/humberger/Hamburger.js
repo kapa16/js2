17da27975058cc31d8a58a82fdd160ac6c66ff73
kapa <kapa16@inbox.ru>
2018-12-23 02:22:11 +0500

    task3

--- lesson1/humberger/Hamburger.js
+++ lesson1/humberger/Hamburger.js
@@ -1,40 +1,17 @@
-const settingsHamburger = {
-  size: {
-    small: {price: 50, calories: 20},
-    large: {price: 100, calories: 40}
-  },
-  stuffing: {
-    cheese: {price: 10, calories: 20},
-    salad: {price: 20, calories: 5},
-    potato: {price: 15, calories: 10}
-  },
-  topping: {
-    mayo: {price: 20, calories: 5},
-    spice: {price: 15, calories: 0}
-  },
-};
-
+/**
+ * Класс для гамбургера
+ */
 class Hamburger {
-  constructor(size, stuffing) {
-    this.settings = settingsHamburger;
-    this.size = size;
-    this.stuffing = stuffing;
-    this.topping = [];
+  constructor(settings) {
+    this.settings = settings;
   }
 
-  addTopping(topping) {
-    if (!this.topping.find(topping)) {
-      this.topping.push(topping);
-    }
-  }
-
-  removeTopping(topping) {
-    const index = this.topping.indexOf(topping);
-    if (!index < 0) {
-      this.topping.splice(index, 1);
-    }
-  }
-
+  /**
+   * рассчитывает значение
+   * @param {string} value - название рассчитываемого значения
+   * @returns {number} - числовое значение
+   * @private
+   */
   _calculate(value) {
     let sum = 0;
     for (const setting in this.settings) {
@@ -51,11 +28,19 @@ class Hamburger {
     return sum;
   }
 
+  /**
+   * Расчитывает цену
+   * @returns {number}
+   */
   calculatePrice() {
     return this._calculate('price');
   }
 
-  calculatecalories() {
+  /**
+   * Рассчитывает калорийность
+   * @returns {number}
+   */
+  calculateCalories() {
     return this._calculate('calories');
   }
 }
---
+++ lesson1/humberger/settingsHamburger.js
@@ -0,0 +1,28 @@
+
+const settingsHamburger = {
+  size: {
+    multiCheck: false,
+    title: 'Размер',
+    small: {price: 50, calories: 20, title: 'Маленький'},
+    large: {price: 100, calories: 40, title: 'Большой'}
+  },
+  stuffing: {
+    multiCheck: false,
+    title: 'Начинка',
+    cheese: {price: 10, calories: 20, title: 'Сыр'},
+    salad: {price: 20, calories: 5, title: 'Салат'},
+    potato: {price: 15, calories: 10, title: 'Картофель'}
+  },
+  topping: {
+    multiCheck: true,
+    title: 'Дополнительно',
+    mayo: {price: 20, calories: 5, title: 'Майонез'},
+    spice: {price: 15, calories: 0, title: 'Приправа'}
+  },
+  bred: {
+    multiCheck: true,
+    title: 'Хлеб',
+    bred1: {price: 20, calories: 5, title: 'Хлеб 1'},
+    bred2: {price: 15, calories: 0, title: 'Хлеб 2'}
+  },
+};
