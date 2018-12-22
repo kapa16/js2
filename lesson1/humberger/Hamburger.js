1579cfa69df81a110125cdc0487ee0977daee1e7
kapa16 <kapa16@inbox.ru>
2018-12-22 23:35:23 +0500

    task3

--- lesson1/humberger/Hamburger.js
+++ lesson1/humberger/Hamburger.js
@@ -1,37 +1,16 @@
 const settingsHamburger = {
   size: {
-    small: {
-      price: 50,
-      calories: 20
-    },
-    large: {
-      price: 100,
-      calories: 40
-    }
+    small: {price: 50, calories: 20},
+    large: {price: 100, calories: 40}
   },
   stuffing: {
-    cheese: {
-      price: 10,
-      calories: 20
-    },
-    salad: {
-      price: 20,
-      calories: 5
-    },
-    potato: {
-      price: 15,
-      calories: 10
-    }
+    cheese: {price: 10, calories: 20},
+    salad: {price: 20, calories: 5},
+    potato: {price: 15, calories: 10}
   },
   topping: {
-    mayo: {
-      price: 20,
-      calories: 5
-    },
-    spice: {
-      price: 15,
-      calories: 0
-    }
+    mayo: {price: 20, calories: 5},
+    spice: {price: 15, calories: 0}
   },
 };
 
@@ -40,27 +19,43 @@ class Hamburger {
     this.settings = settingsHamburger;
     this.size = size;
     this.stuffing = stuffing;
-    this._toppings = [];
+    this.topping = [];
   }
 
   addTopping(topping) {
-    if (!this._toppings.find(topping)) {
-      this._toppings.push(topping);
+    if (!this.topping.find(topping)) {
+      this.topping.push(topping);
     }
   }
 
-  removeTopping() {
-    const index = this._toppings.indexOf(topping);
+  removeTopping(topping) {
+    const index = this.topping.indexOf(topping);
     if (!index < 0) {
-      this._toppings.splice(index, 1);
+      this.topping.splice(index, 1);
     }
+  }
+
+  _calculate(value) {
+    let sum = 0;
+    for (const setting in this.settings) {
+      const currentSetting = settingsHamburger[setting];
+      const property = this[setting];
+      if (Array.isArray(property)) {
+        property.forEach(prop => {
+          sum += currentSetting[prop][value];
+        });
+      } else {
+        sum += currentSetting[property][value];
+      }
+    }
+    return sum;
   }
 
   calculatePrice() {
-
+    return this._calculate('price');
   }
 
-  calculateCalorific() {
-
+  calculatecalories() {
+    return this._calculate('calories');
   }
 }
