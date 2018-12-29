f9fd778ab4895d5f4ea99e19f9707296158e953a
Pavel <kapa16@inbox.ru>
2018-12-29 13:51:00 +0500

    lesson3

---
+++ lesson3/homework2/Gallery.js
@@ -0,0 +1,67 @@
+class Modal {
+    constructor(modal){
+        this.settings = modal;
+        this.btn = null;
+        this.imgWrap = null;
+        this.block = null;
+        this._build();
+    }
+    _build(){
+        let template =  `
+                    <div id="${this.settings.id}" class="modal-wrap">
+                        <div class="top-block">
+                            <button type="button" class="${this.settings.btn}" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        </div>
+                        <div class="${this.settings.imgWrap}"></div>
+                    </div>`;
+        document.body.innerHTML += template;
+        this.imgWrap = document.querySelector(`.${this.settings.imgWrap}`);
+        this.block = document.querySelector(`#${this.settings.id}`);
+        this._initElements();
+    }
+    _initElements(){
+        this.btn = document.querySelector(`.${this.settings.btn}`);
+        this.btn.addEventListener('click', () => {
+            this.block.classList.remove('show');
+            document.body.classList.remove('no-scroll');
+        })
+    }
+    open(element){
+        this._addImage(element.src, element.alt);
+        this.block.classList.add('show');
+        document.body.classList.add('no-scroll');
+    }
+    _addImage(src, alt){
+        this.imgWrap.innerHTML = `<img src="${src}" alt="${alt}">`
+    }
+}
+class Gallery {
+    constructor(source, container, modal = {id: 'modal', imgWrap: 'img-wrap', btn: 'close'}){
+        this.container = container;
+        this.modal = new Modal(modal);
+        this._getData(source);
+    }
+    _getData(source){
+        fetch(source)
+            .then(result => result.json())
+            .then(data => this._buildGallery(data));
+    }
+    _buildGallery(data){
+       this.container = document.querySelector(this.container);
+       for (let image of data){
+           this._addImage(image);
+       }
+       this._initGallery();
+    }
+    _addImage(image){
+        this.container.innerHTML += `<img src="${image.src}" alt="${image.alt}">`
+    }
+    _initGallery(){
+        this.container.addEventListener('click', e => {
+            if (e.target.tagName !== 'IMG'){
+                return
+            }
+            this.modal.open(e.target);
+        })
+    }
+}
