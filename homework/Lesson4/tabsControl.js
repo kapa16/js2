123883d68262e87ea91b0cc2e912360101c011c6
Pavel <kapa16@inbox.ru>
2019-01-14 11:51:00 +0500

    task1

--- homework/Lesson4/tabsControl.js
+++ homework/Lesson4/tabsControl.js
@@ -1,26 +1,50 @@
 "use strict";
 
 class TabsControl {
-  constructor(settings) {
+  constructor(parentBlock, settings) {
+    this.parentBlock = parentBlock;
     this.settings = {
-      tabsClass: 'tabs',
       tabsControlClass: 'tabs__control',
+      tabsContentClass: 'tabs__content',
       activeTabClass: 'tabs__active'
     };
     Object.assign(this.settings, settings);
+    this._showContent(0);
     this._addEventListeners();
   }
 
+  /**
+   * Устанвливает обработчики собыйти на переключатели закладок
+   * @private
+   */
   _addEventListeners() {
-    $(`.${this.settings.tabsClass} .${this.settings.tabsControlClass}`)
+    $(this.parentBlock).find(`.${this.settings.tabsControlClass}`)
       .on('click', (evt) => this._onControlClick(evt));
   }
 
+  /**
+   * Обработчик события нажатия на переключатьль закладки
+   * @param {Event} evt - событие клика на переключатель
+   * @private
+   */
   _onControlClick(evt) {
     const currentEl = evt.currentTarget;
-    $(`.${this.settings.tabsClass} .${this.settings.activeTabClass}`)
+    $(this.parentBlock).find(`.${this.settings.activeTabClass}`)
       .removeClass(this.settings.activeTabClass);
 
-    $(currentEl).parent().addClass(this.settings.activeTabClass);
+    $(currentEl).addClass(this.settings.activeTabClass);
+    const tabNumber = $(this.parentBlock).find(`.${this.settings.tabsControlClass}`).index(currentEl);
+
+    this._showContent(tabNumber);
+  }
+
+  /**
+   * Отображение открытой закладки
+   * @param {int} tabNumber - номер открытой вкладки
+   * @private
+   */
+  _showContent(tabNumber) {
+    $(this.parentBlock).find(`.${this.settings.tabsContentClass}`)
+      .hide().eq(tabNumber).show();
   }
 }
