dd3386dc7c1213f8d6198573fc6341bebdd29e6f
Pavel <kapa16@inbox.ru>
2019-01-14 13:40:50 +0500

    1

---
+++ homework/Lesson4/formValidate/AutoCompleteField.js
@@ -0,0 +1,24 @@
+"use strict";
+
+class DropListControl{
+  constructor(dropListId) {
+    this.dropListId = dropListId;
+    this.cities = [];
+    this._getCitiesList();
+  }
+
+  _getCitiesList() {
+    fetch('cities.json')
+      .then(response => response.json())
+      .then(cities => this._fillDropList(cities))
+      .catch(ex => console.error('Error file load', ex));
+  }
+
+  _fillDropList(cities) {
+    this.cities = cities;
+    $(this.dropListId).append('<option disabled selected>Выберите город</option>');
+    for (const city of cities) {
+      $(this.dropListId).append(`<option value=${city.name}>${city.name}</option>`);
+    }
+  }
+}
