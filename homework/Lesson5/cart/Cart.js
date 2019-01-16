24c640f634d28de2bfc3077e4b28af9a2fc8b3fb
Pavel <kapa16@inbox.ru>
2019-01-16 11:44:05 +0500

    task1

--- homework/Lesson5/cart/Cart.js
+++ homework/Lesson5/cart/Cart.js
@@ -9,6 +9,7 @@ class Cart {
     }
     _init(){
         this._render();
+        this._addEventHandlers();
         fetch(this.source)
             .then(result => result.json())
             .then(data => {
@@ -36,14 +37,29 @@ class Cart {
         $totalGoods.appendTo($(this.container));
         $totalPrice.appendTo($(this.container));
     }
+    _addEventHandlers(){
+        $(this.container).on('click', '.reduce-quantity', evt => this._onChangeQuantity(evt,-1));
+        $(this.container).on('click', '.increase-quantity', evt => this._onChangeQuantity(evt, 1));
+        $(this.container).on('click', '.delete-product', evt => this._onChangeQuantity(evt, 0, true));
+    }
     _renderItem(product){
         let $container = $('<div/>', {
             class: 'cart-item',
             'data-product': product.id_product
         });
         $container.append($(`<p class="product-name">${product.product_name}</p>`));
-        $container.append($(`<p class="product-quantity">${product.quantity}</p>`));
+
+        const $quantity = $('<div/>', {
+            class: 'quantity-wrap'
+        });
+
+        $quantity.append($(`<button class="btn-quantity reduce-quantity">-</button>`));
+        $quantity.append($(`<p class="product-quantity">${product.quantity}</p>`));
+        $quantity.append($(`<button class="btn-quantity increase-quantity">+</button>`));
+        $container.append($quantity);
+
         $container.append($(`<p class="product-price">${product.price} руб.</p>`));
+        $container.append($(`<button class="btn-quantity delete-product">X</button>`));
         $container.appendTo($('.cart-items-wrap'));
     }
     _renderSum(){
@@ -55,14 +71,14 @@ class Cart {
         $container.find('.product-quantity').text(product.quantity);
         $container.find('.product-price').text(`${product.quantity*product.price} руб.`);
     }
+    _getCartItem(id) {
+        return this.cartItems.find(product => product.id_product === id);
+    }
     addProduct(element) {
         let productId = +$(element).data('id');
-        let find = this.cartItems.find(product => product.id_product === productId);
+        let find = this._getCartItem(productId);
         if(find){
-            find.quantity++;
-            this.countGoods++;
-            this.amount += find.price;
-            this._updateCart(find);
+            this._changeQuantity(find, 1);
         } else {
             let product = {
                 id_product: productId,
@@ -77,7 +93,28 @@ class Cart {
         }
         this._renderSum();
     }
+    _getEventProductId(evt){
+        return $(evt.target).closest('.cart-item').data('product');
+    }
+    _onChangeQuantity(evt, quantity = 1, deleteItem = false){
+        let find = this._getCartItem(this._getEventProductId(evt));
+        this._changeQuantity(find, deleteItem ? -find.quantity : quantity);
+    }
+    _changeQuantity(cartItem, quantity) {
+        cartItem.quantity += quantity;
+        this.countGoods += quantity;
+        this.amount += cartItem.price * quantity;
+        if (cartItem.quantity === 0) {
+            this._remove(cartItem.id_product)
+        } else {
+            this._updateCart(cartItem);
+        }
+        this._renderSum();
+    }
     _remove(id){
-        //TODO: удаление товара
+        let find = this._getCartItem(id);
+        this.cartItems.splice(this.cartItems.indexOf(find), 1);
+        let $container = $(`div[data-product="${id}"]`);
+        $container.remove();
     }
 }
